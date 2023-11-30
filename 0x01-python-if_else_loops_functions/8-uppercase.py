a2c6663679d8074ded20b7cba113ebf02e9a74e4
SantosAndrewz <sentosandrewz@gmail.com>
2023-11-29 18:23:18 -0800

    change str to uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    ucase_str = ""
+    for c in str:
+        if 97 <= ord(c) <= 122:
+            ucase_c = chr(ord(c) - 32)
+            ucase_str = ucase_str + ucase_c
+        else:
+            ucase_str = ucase_str + c
+    print("{}\n".format(ucase_str))
