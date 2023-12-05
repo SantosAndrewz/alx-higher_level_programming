f868346fd9da4395d9fc8e6a40c50b745c7212f5
SantosAndrewz <sentosandrewz@gmail.com>
2023-12-04 19:00:03 -0800

    sorted list

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if not  my_list:
+        return None
+    else:
+        my_newlist = sorted(my_list, reverse=True)
+        return (my_newlist[0])
