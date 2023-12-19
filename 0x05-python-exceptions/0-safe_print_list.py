ef89372c862ab19ebc479a86d01034edef188c19
SantosAndrewz <sentosandrewz@gmail.com>
2023-12-18 17:44:47 -0800

    safe_print_list2

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,5 +7,5 @@ def safe_print_list(my_list=[], x=0):
             count_elmt += 1
         except IndexError:
             pass
-        print()
-        return count_elmt
+    print()
+    return count_elmt
