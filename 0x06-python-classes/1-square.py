1a8316ff5bf3ea12ecf9b1510d2a27050bfa5ca2
SantosAndrewz <sentosandrewz@gmail.com>
2024-01-17 23:13:41 -0800

    initializing the square with size_1.

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 """Creates and defines a class Square."""
 
+
 class Square:
     """Creates a class - Square."""
 
-
-    def __init__ (self, size):
+    def __init__(self, size):
         """Initializing a class Square.
 
-
         Args:
-            size (int): Shows the size of the new Square.
+            size(int): Shows the size of the new Square.
         """
 
         self.__size = size
