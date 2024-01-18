24ab9c79f3715f835efae6d96d87ea993a6e5dd5
SantosAndrewz <sentosandrewz@gmail.com>
2024-01-18 06:33:49 -0800

    coordinates for the position of the square.

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -36,13 +36,13 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if not isinstance(value, tuple) or (len(value) != 2 
-                or not all(isinstance(n, int) for n in value) 
-                    or not all(n >= 0 for n in value)):
-                raise TypeError("position must be a tuple of 2 positive integers")
+        """Setting the position of the square to value."""
+        if not isinstance(value, tuple)
+                or (len(value) != 2
+                or not all(isinstance(n, int) for n in value)
+                or not all(n >= 0 for n in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
-
-
 
     def area(self):
         """Returns the area of the square."""
