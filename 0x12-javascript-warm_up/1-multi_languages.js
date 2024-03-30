56642f096d4409a44fdcb499cdfd33195b6f5c95
SantosAndrewz <sentosandrewz@gmail.com>
2024-03-30 16:35:12 -0700

    The three languages

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const firstMessage = 'C is fun';
+const secondMessage = 'Python is cool';
+const thirdMessage = 'JavaScript is amazing';
+
+console.log(firstMessage);
+console.log(secondMessage);
+console.log(thirdMessage);
