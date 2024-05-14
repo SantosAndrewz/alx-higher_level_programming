fc83c498c11396f9342af2fbd1c9155df4648fb4
SantosAndrewz <sentosandrewz@gmail.com>
2024-05-14 07:08:57 -0700

    modified status code file

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script takes in a URL, sends it a request and displays only the response's status code.
-curl "$1"-s -o /dev/null -w "%{http_code}"
+curl "$1" -s -o /dev/null -w "%{http_code}"
