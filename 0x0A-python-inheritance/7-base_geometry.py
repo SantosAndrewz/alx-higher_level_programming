ca4c5e6d4cb6c1c0ad3d2308b514c18be3f4264b
SantosAndrewz <sentosandrewz@gmail.com>
2024-06-19 09:45:17 -0700

    integer validator

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -30,7 +30,7 @@ class BaseGeometry:
             ValueError: If value is less than or equal to 0.
         '''
 
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError(f'{name} must be an integer')
 
         if value <= 0:
