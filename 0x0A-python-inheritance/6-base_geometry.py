fe2a124fdba0624adb12deff7129d4fb689b7cbb
SantosAndrewz <sentosandrewz@gmail.com>
2024-06-19 02:26:10 -0700

    inprove on the class base

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''
+Module for a class BaseGeometry.
+'''
+
+
+class BaseGeometry:
+    ''' creates a class BaseGeometry '''
+
+    def area(self):
+        '''
+        Public instance method that raises an Exception.
+
+        Raises:
+            Exception: display message 'area() is not implemented'.
+        '''
+
+        raise Exception('area() is not implemented')
