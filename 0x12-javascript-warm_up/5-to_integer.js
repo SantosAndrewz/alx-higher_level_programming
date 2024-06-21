c33da70949e0e423018cc929dca9fbe88d1f0411
SantosAndrewz <sentosandrewz@gmail.com>
2024-06-21 14:29:16 -0700

    corrected typo

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -5,7 +5,6 @@ const convArg = parseInt(process.argv[2]);
 
 if (isNaN(convArg)) {
   console.log('Not a number');
-}
-else {
-  console.log('My number: ' + conv_arg);
+} else {
+  console.log('My number: ' + convArg);
 }
