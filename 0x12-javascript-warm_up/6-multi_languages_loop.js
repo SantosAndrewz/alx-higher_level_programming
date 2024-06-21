f94954397744d50dd2896bd99a53e1cd36933f05
SantosAndrewz <sentosandrewz@gmail.com>
2024-06-21 13:54:05 -0700

    looping through multi-languages

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// script prints 3 lines using an array of string and a loop
+
+
+const strs = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+for (let x = 0; x < strs.length; x++) {
+	console.log(strs[x]);
+}
