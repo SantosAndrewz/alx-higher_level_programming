9dc6240812ad252e10972b9236232c1262bf658c
SantosAndrewz <sentosandrewz@gmail.com>
2024-06-21 13:34:50 -0700

    print integer

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// script prints my number
+
+const conv_arg = psrseInt(process.argv[2]);
+
+if (isNaN(conv_arg)) {
+	console.log('Not a number');
+}
+else {
+	console.log('My number: ' + conv_arg);
+}
