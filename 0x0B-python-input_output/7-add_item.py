dfb418d761f8f23f5788463b4c1f9f3b985b07a1
SantosAndrewz <sentosandrewz@gmail.com>
2024-06-21 02:44:20 -0700

    corrected documentation

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,8 +6,6 @@ Module adds all arguments to a Python list, and then save them to a file.
 import os
 import sys
 
-
-
 save_jsn = __import__('5-save_to_json_file').save_to_json_file
 load_jsn = __import__('6-load_from_json_file').load_from_json_file
 
