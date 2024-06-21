3c4c0e927f5b0d62e44774939ed98792f36e21a3
SantosAndrewz <sentosandrewz@gmail.com>
2024-06-21 14:16:53 -0700

    corrected format

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 // script prints my number
 
-const conv_arg = parseInt(process.argv[2]);
+const convArg = parseInt(process.argv[2]);
 
-if (isNaN(conv_arg)) {
-	console.log('Not a number');
+if (isNaN(convArg)) {
+  console.log('Not a number');
 }
 else {
-	console.log('My number: ' + conv_arg);
+  console.log('My number: ' + conv_arg);
 }
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 // script prints 3 lines using an array of string and a loop
 
-
-const strs = ["C is fun", "Python is cool", "JavaScript is amazing"];
+const strs = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let x = 0; x < strs.length; x++) {
-	console.log(strs[x]);
+  console.log(strs[x]);
 }
