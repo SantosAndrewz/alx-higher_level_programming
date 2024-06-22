4faae4a943d31b947839a089b24573cdbf05a06c
SantosAndrewz <sentosandrewz@gmail.com>
2024-06-21 17:27:58 -0700

    print a square

---
+++ 0x12-javascript-warm_up/square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// a script printing a square.
+
+const size = parseInt(process.argv[2]);
+
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (let x = 0; x < size; x++) {
+    console.log('X.repeat(size));
+  }
+}
